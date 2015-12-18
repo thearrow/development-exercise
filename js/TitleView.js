598f4f6f60650d82b85c73f46da6c28b196bd170
Jake Klingler <thearrow3456@gmail.com>
2015-12-17 23:28:35 -0700

    multi-line SVG mask for transparent text

--- js/TitleView.js
+++ js/TitleView.js
@@ -3,16 +3,63 @@ import React, { Component } from 'react';
 export class TitleView extends Component {
   constructor(props) {
     super(props);
+    this.lines = this.buildLines();
+  }
+
+  /**
+   * Split the title into roughly 15-character-long lines for SVG mask
+   * @return {string[]} string array of title lines
+   */
+  buildLines() {
+    let words = this.props.text.split(' ') || [];
+    let line = '';
+    let lines = [];
+    let counter = 0;
+    let limit = 15;
+
+    for (let word of words) {
+      if (counter < limit) {
+        line += word + ' ';
+        counter += word.length + 1;
+      } else {
+        lines.push(line);
+        counter = word.length + 1;
+        line = word + ' ';
+      }
+    }
+    lines.push(line);
+
+    return lines;
   }
 
   render() {
+    let titleLines = [];
+    for (let i = 0; i < this.lines.length; i++) {
+      titleLines.push(
+        <text
+          id={'title' + (i+1)}
+          key={i}
+          className='titleLine'
+          x="10"
+          y={(i+1) + 'em'}>
+            {this.lines[i]}
+        </text>
+      );
+    }
+
     return (
       <div>
         <div className='button pencil'
           onClick={this.props.handleEdit}/>
-        <h1>
-          {this.props.text}
-        </h1>
+        <svg className='title' style={{height: (this.lines.length + .4) + 'em'}}>
+          <defs>
+            <mask id="mask" x="0" y="0" width="100%" height="100%" >
+              <rect id="alpha" x="0" y="0" width="100%" height="100%"/>
+              {titleLines}
+            </mask>
+          </defs>
+          <rect id="base" x="0" y="0" width="100%" height="100%"/>
+        </svg>
       </div>
     );
   }
